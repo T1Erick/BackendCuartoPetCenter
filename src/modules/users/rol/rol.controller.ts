0de18df5fa58915599e8782b23cdecc7c7b755ff
T1Erick <guevaraerick49@gmail.com>
2023-06-19 20:56:52 -0500

    Erick servicios y controler de bills y rol

--- src/modules/users/rol/rol.controller.ts
+++ src/modules/users/rol/rol.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { RolService } from './rol.service';
+import { RolEventDto } from './dto/rol.dto';
+import { UpdateRolDto } from './dto/update.rol.dto';
 
 @Controller('rol')
-export class RolController {}
+export class RolController {
+    constructor(private readonly rolService: RolService) {}
+
+    @Post()
+    async create(@Body() CreateRolDto:RolEventDto){
+        return await this.rolService.create(CreateRolDto);
+    }
+
+
+    @Get()
+    async findAll(){
+        return await this.rolService.findAll()
+    }
+    @Patch(':id')
+    async update(@Param('id') id:string,@Body() updateRolDto: UpdateRolDto){
+        return await this.rolService.update(id, updateRolDto)
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id:string){
+        return await this.rolService.remove(id);
+    }
+}
--- src/modules/users/rol/rol.service.ts
+++ src/modules/users/rol/rol.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { rolEntity } from './entities/rol.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { RolEventDto } from './dto/rol.dto';
+import { ErrorManager } from 'src/utils/error.manage';
+import { UpdateRolDto } from './dto/update.rol.dto';
 
 @Injectable()
-export class RolService {}
+export class RolService {
+
+    constructor(
+        @InjectRepository(rolEntity) private rolEntityDto: Repository<rolEntity>
+    ){}
+
+    async create(createroleventDto: RolEventDto):Promise<rolEntity>{
+        try {
+            const rolcreate: rolEntity = await this.rolEntityDto.save(createroleventDto);
+            return rolcreate;}catch(e){
+                throw new Error(e)
+            }
+    }
+
+    async findAll():Promise<rolEntity[]>{
+        try{
+            const rolall: rolEntity[]= await this.rolEntityDto.find();
+            if(rolall.length ===0){
+                throw new ErrorManager({
+                    type:'BAD_REQUEST',
+                    message:'No existen secciones registradas'
+                });
+
+            }
+            return rolall
+        }catch (e){
+            throw ErrorManager.createSignatureError(e.message)
+        }
+    }
+
+    async update(id:string, updateroldto: UpdateRolDto):Promise<UpdateResult| undefined>{
+        try{
+            const updaterol: UpdateResult= await this.rolEntityDto.update(id,updateroldto);
+            if(updaterol.affected ===0){
+                throw new ErrorManager({
+                    type:'BAD_REQUEST',
+                    message:'La seccion no se pudo actualizar',
+                });
+            }
+            return updaterol;
+        }catch(e){
+            throw ErrorManager.createSignatureError(e.message)
+        }
+    }
+    async remove(id:string):Promise<DeleteResult | undefined>{
+        try{
+            const deleterol: DeleteResult = await this.rolEntityDto.delete(id);
+            if(deleterol.affected ===0){
+                throw new ErrorManager({
+                    type:'BAD_REQUEST',
+                        message:'La seccion no se pudo eliminar',
+
+                })
+            }
+            return deleterol;
+        }catch (e){
+            throw ErrorManager.createSignatureError(e.message)
+        }
+    }
+}
